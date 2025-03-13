a726d079404ef0a8b3b4566705e3eead8ec674ad
JonatanDvOrtiz <jdortizc3@academia.usbbog.edu.co>
2025-03-13 18:54:20 -0500

    Create Calculadora1.py

---
+++ Calculadora1.py
@@ -0,0 +1,23 @@
+#Ejercicio 1: calculadora básica
+f1= int (input("escriba un numero entero "))
+f2= int(input("escriba otro numero entero "))
+
+resultado= f1+f2
+
+#print#
+print(f"La suma de {f1} y {f2} es {resultado}")
+
+resultado= f1-f2
+
+#print#
+print(f"La resta de {f1} y {f2} es {resultado}")
+
+resultado= f1*f2
+
+#print#
+print(f"La multiplicación de {f1} y {f2} es {resultado}")
+
+resultado= f1/f2
+
+#print#
+print(f"La división de {f1} y {f2} es {resultado}")
