a49f41a2302e2e998b3f36aeb80b238173399f91
JonatanDvOrtiz <jdortizc3@academia.usbbog.edu.co>
2025-04-24 18:51:50 -0500

    Update Calculadora2.py

--- Calculadora2.py
+++ Calculadora2.py
@@ -1,32 +1,51 @@
+"""
+Título de práctica: Calculadora1
+
+Calculadora con selección de operación
+
+Autor: ElAutor <jdortizc3@academia.usbbog.edu.co>
+Fecha: 2025-03-20
+"""
 print("Calculadora básica")
 
-# Convertimos las entradas a números
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
-operador = input("> Escriba 1 para suma, 2 para resta, 3 para multiplicación, 4 para división, 5 para potenciación, 6 para módulo, 7 para salir: ")
-
-if operador == '1':
-    resultado = num1 + num2
-    print(f"El resultado de {num1} + {num2} es {resultado}")
-elif operador == '2':
-    resultado = num1 - num2
-    print(f"El resultado de {num1} - {num2} es {resultado}")
-elif operador == '3':
-    resultado = num1 * num2
-    print(f"El resultado de {num1} * {num2} es {resultado}")
-elif operador == '4':
-    if num2 != 0:
-        resultado = num1 / num2
-        print(f"El resultado de {num1} / {num2} es {resultado}")
+for _ in range(1000):  # Bucle
+    # Ingreso de operandos
+    num1 = float(input("Ingrese el primer operando: "))
+    num2 = float(input("Ingrese el segundo operando: "))
+    operador = input("""
+            Indique la operación a realizar:
+         seleccione 1) Sumar
+         seleccione 2) Restar
+         seleccione 3) Multiplicar
+         seleccione 4) Dividir
+         seleccione 5) Potenciación
+         seleccione 6) Módulo
+         seleccione 7) Salir    
+    """)
+    #Ejecuta operación indicada
+    if operador == '1':
+        resultado = num1 + num2
+        print(f"El resultado de {num1} + {num2} es {resultado}")
+    elif operador == '2':
+        resultado = num1 - num2
+        print(f"El resultado de {num1} - {num2} es {resultado}")
+    elif operador == '3':
+        resultado = num1 * num2
+        print(f"El resultado de {num1} * {num2} es {resultado}")
+    elif operador == '4':
+        if num2 != 0:
+            resultado = num1 / num2
+            print(f"El resultado de {num1} / {num2} es {resultado}")
+        else:
+            print("Error: No se puede dividir entre cero.")
+    elif operador == '5':
+        resultado = num1 ** num2
+        print(f"El resultado de {num1} elevado a {num2} es {resultado}")
+    elif operador == '6':
+        resultado = num1 % num2
+        print(f"El módulo de {num1} % {num2} es {resultado}")
+    elif operador == '7':
+        print("Saliendo del programa.")
+        break
     else:
-        print("Error: No se puede dividir entre cero.")
-elif operador == '5':
-    resultado = num1 ** num2
-    print(f"El resultado de {num1} elevado a {num2} es {resultado}")
-elif operador == '6':
-    resultado = num1 % num2
-    print(f"El módulo de {num1} % {num2} es {resultado}")
-elif operador == '7':
-    print("Saliendo del programa.")
-else:
-    print("Opción no válida.")
+        print("Opción no válida.")
